96ac741e8ea1c71f662ec582deb2b636e2890f6b
Tomlevron <54799120+Tomlevron@users.noreply.github.com>
2020-11-29 13:03:43 +0200

    Update HW2.py

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -5,42 +5,33 @@ This script is for the 2nd homework assigment
 @author: toml
 """
 import numpy as np
-import cv2
+import cv2 as cv
+from matplotlib import pyplot as plt
 
-img = cv2.imread('Gonz.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv.imread('Gonz.jpg', cv.IMREAD_GRAYSCALE)
 
 # kernel size: The greater the size, the more the motion. 
 kernel_size = 10
   
-# Create the vertical kernel. 
+# Create the kernals
 kernel_v = np.zeros((kernel_size, kernel_size)) 
-  
-# Create a copy of the same for creating the horizontal kernel. 
 kernel_h = np.copy(kernel_v) 
   
-# Fill the middle row with ones. 
+# Filling of the the middle row with ones. 
 kernel_v[:, int((kernel_size - 1)/2)] = np.ones(kernel_size) 
 kernel_h[int((kernel_size - 1)/2), :] = np.ones(kernel_size) 
   
-# Normalize. 
+# Normalize kernals. 
 kernel_v /= kernel_size 
 kernel_h /= kernel_size 
   
-# Apply the vertical kernel. 
-vertical_mb = cv2.filter2D(img, -1, kernel_v) 
-  
-# Apply the horizontal kernel. 
-horizonal_mb = cv2.filter2D(img, -1, kernel_h) 
+# convolution of the image with the kernal
+vertical_mb = cv.filter2D(img, -1, kernel_v) 
+horizonal_mb = cv.filter2D(img, -1, kernel_h) 
 
-
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imshow('Blurred',vertical_mb)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imshow('Blurred',horizonal_mb)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Plot
+plt.subplot(121),plt.imshow(img,cmap='gray'),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(vertical_mb,cmap='gray'),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
