161fecd90f2cda725abc3df401b73c4e027669b2
Tomlevron <54799120+Tomlevron@users.noreply.github.com>
2020-11-29 15:29:58 +0200

    Update HW2.py

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -7,31 +7,52 @@ This script is for the 2nd homework assigment
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from scipy.signal import convolve2d as conv2
 
 img = cv.imread('Gonz.jpg', cv.IMREAD_GRAYSCALE)
 
-# kernel size: The greater the size, the more the motion. 
-kernel_size = 10
+def create_kernal(size,horizontal=False):
+    '''This function is creating a normalized kernal
+    kernal is vertical by defult, enter False for horizntal'''
+    kernel = np.zeros((size, size))
+    
+    if horizontal: #vertical
+        kernel[:, int((size - 1)/2)] = np.ones(size)
+    else: #horizontal
+        kernel[int((size - 1)/2), :] = np.ones(size)  
+    
+    kernel /= size 
+    return kernel
   
 # Create the kernals
-kernel_v = np.zeros((kernel_size, kernel_size)) 
-kernel_h = np.copy(kernel_v) 
-  
-# Filling of the the middle row with ones. 
-kernel_v[:, int((kernel_size - 1)/2)] = np.ones(kernel_size) 
-kernel_h[int((kernel_size - 1)/2), :] = np.ones(kernel_size) 
-  
-# Normalize kernals. 
-kernel_v /= kernel_size 
-kernel_h /= kernel_size 
+kernel_v = create_kernal(10)
+kernel_h = create_kernal(10, horizontal=True) 
   
 # convolution of the image with the kernal
-vertical_mb = cv.filter2D(img, -1, kernel_v) 
-horizonal_mb = cv.filter2D(img, -1, kernel_h) 
+vertical_mb = conv2(img, kernel_v, mode='same') 
+horizontal_mb = conv2(img, kernel_h, mode='same') 
+
+both = conv2(img, kernel_v, mode='same')
+blur = cv.blur(img,(10,10))
+
 
 # Plot
-plt.subplot(121),plt.imshow(img,cmap='gray'),plt.title('Original')
+plt.figure
+plt.imshow(img,cmap='gray'),plt.title('Orignal')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(vertical_mb,cmap='gray'),plt.title('Averaging')
+plt.show()
+
+
+plt.figure
+plt.subplot(121),plt.imshow(blur,cmap='gray'),plt.title('Blur')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(both,cmap='gray'),plt.title('conv2d')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+plt.figure
+plt.subplot(121),plt.imshow(horizontal_mb,cmap='gray'),plt.title('Horzintal')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(vertical_mb,cmap='gray'),plt.title('Vertical')
 plt.xticks([]), plt.yticks([])
 plt.show()
