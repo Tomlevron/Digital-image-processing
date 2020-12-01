9b7eca96a7fd1014ef2143b4e4f276d0f9f41e51
Tomlevron <54799120+Tomlevron@users.noreply.github.com>
2020-12-01 12:20:36 +0200

    Update HW2_script.py

--- HW2/HW2_script.py
+++ HW2/HW2_script.py
@@ -5,3 +5,15 @@ Created on Tue Dec  1 11:28:39 2020
 @author: toml
 """
 
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+from scipy.signal import convolve2d as conv2
+from numpy.fft import fft2, ifft2, ifftshift
+from skimage import restoration
+from scipy import ndimage
+from numpy import sum,sqrt
+from numpy.random import standard_normal
+
+img = cv.imread('Gonz.jpg', cv.IMREAD_GRAYSCALE) # BGR and [Cols,Rows]
+
