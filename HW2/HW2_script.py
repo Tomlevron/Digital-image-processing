08bf44cb380794a2a6728554ea105c31ed70f420
Tomlevron <54799120+Tomlevron@users.noreply.github.com>
2020-12-03 09:13:04 +0200

    Update HW2_script.py

--- HW2/HW2_script.py
+++ HW2/HW2_script.py
@@ -8,13 +8,9 @@ Created on Tue Dec  1 11:28:39 2020
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-from scipy.signal import convolve2d as conv2
-from numpy.fft import fft2, ifft2, ifftshift,fftshift
-from skimage import restoration
+# from scipy.signal import convolve2d as conv2
+from numpy.fft import fft2, ifft2, ifftshift#,fftshift
 from scipy import ndimage
-from numpy import sum,sqrt
-from numpy.random import standard_normal
-from skimage.util import random_noise 
 
 
 
